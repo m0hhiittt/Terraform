250e462803a0ebd231c5104e31c08dcd36d52928
ArtyaSoft <ArtyaSoft@DESKTOP-LSUNA3M>
2026-09-14 14:30:16 +0530

    added veriables

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  default = "East Us"
+}
+
+variable "resource_group_name" {
+  default = "terraform-nginx-rg"
+}
+
+variable "vm_name" {
+  default = "nginx-vm"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "Password1234!"
+}
